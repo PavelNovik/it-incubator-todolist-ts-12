d8ac90e97d1e50fe4acaa24ce30147accd116126
Pavel Novik <brn2befree@gmail.com>
2023-11-30 18:33:44 +0300

    finish class work

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     preset: 'jest-puppeteer',
     testRegex: './*\\.test\\.js$',
     setupFilesAfterEnv: ['./setupTests.js'],
+    testTimeout: 30000,
     // timeout: 50000
 };
